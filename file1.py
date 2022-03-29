552853b11dabe0b87566cbd349e93c33d748c3cd
Valeri <contact@natae.dev>
2022-03-28 18:12:11 -0600

    feat: add new variable

--- file1.py
+++ file1.py
@@ -1,5 +1,6 @@
 
 num1 = int(input('Inserte un numero \n'  ))
 num2 = int(input('Inserte un numero \n'  ))
-suma = num1+num2
+num3 = int(input('Inserte un numero \n'  ))
+suma = num1+num2+num3
 print ('Tu resultado es: ', suma )
